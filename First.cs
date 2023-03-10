ebc50441b49fb213d91eb9b4103d847840444272
mr-hollowman <selvamani825@gmail.com>
2023-03-10 22:41:13 +0530

    initial commit

---
+++ First.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FirstProgram
+{
+    class startingPoint{
+        static void Main(String[] arg){
+            Console.WriteLine("hello world");
+            Console.WriteLine("hello world");
+        }
+    }
+}
